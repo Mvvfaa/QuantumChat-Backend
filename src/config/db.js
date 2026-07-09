1d1fd29acb4701fb90cfad765e588cdd197c5be4
SENODROOM <saadamin691@gmail.com>
2026-07-09 18:06:18 +0500

    fixed

--- src/config/db.js
+++ src/config/db.js
@@ -5,12 +5,21 @@ import mongoose from 'mongoose';
 // reconnect (or race concurrent connects) each time.
 let connectionPromise;
 
+export function buildMongoUri() {
+  if (process.env.MONGODB_URI) return process.env.MONGODB_URI;
+
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_CLUSTER, MONGODB_DB } = process.env;
+  if (!MONGODB_USER || !MONGODB_PASSWORD || !MONGODB_CLUSTER) {
+    throw new Error('Set MONGODB_URI, or MONGODB_USER + MONGODB_PASSWORD + MONGODB_CLUSTER');
+  }
+  const user = encodeURIComponent(MONGODB_USER);
+  const password = encodeURIComponent(MONGODB_PASSWORD);
+  return `mongodb+srv://${user}:${password}@${MONGODB_CLUSTER}/${MONGODB_DB || ''}?retryWrites=true&w=majority&appName=Cluster0`;
+}
+
 export function connectDB() {
   if (!connectionPromise) {
-    const uri = process.env.MONGODB_URI;
-    if (!uri) {
-      throw new Error('MONGODB_URI is not set');
-    }
+    const uri = buildMongoUri();
     connectionPromise = mongoose
       .connect(uri)
       .then(() => console.log(`MongoDB connected: ${mongoose.connection.host}/${mongoose.connection.name}`))
